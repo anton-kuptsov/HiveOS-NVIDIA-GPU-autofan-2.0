1e58931c66a61dad8b14499f1a1db381dd2ca715
Steambot33 <39290991+Steambot33@users.noreply.github.com>
2018-05-30 21:29:32 +0300

    Update autofan.sh

--- autofan.sh
+++ autofan.sh
@@ -132,7 +132,7 @@ while true
 						[[ $MINER_STOP == 1 ]] && ! screen -ls | grep -q "miner" && safe_mode 1
 						MINER_STOP=1
 				fi
-                elif [[ $GPU_TEMP > $MIN_TEMP  &&  $GPU_TEMP < $MAX_TEMP ]]
+                elif [[ $GPU_TEMP > $MIN_TEMP ]] && [[ $GPU_TEMP < $MAX_TEMP ]]
                     then
 						FAN_SPEED=$((  $GPU_TEMP *(($GPU_TEMP - $MIN_TEMP) * 4 + $MIN_COEF)/100 ))
 
